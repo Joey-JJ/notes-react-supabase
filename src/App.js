177077d3351bfeba777a85396d9824f4681a28d0
Joey van der Valk <joeyvandervalk@gmail.com>
2022-10-03 16:54:00 +0200

    initial commit, set up project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { supabase } from "./utils/supabaseClient";
+import { sessionContext } from "./utils/sessionContext";
+import Auth from "./components/Auth";
+import Notes from "./components/Notes";
 
-function App() {
+const App = () => {
+  const [session, setSession] = useState(null);
+
+  // useEffect(() => {
+  //   const fetchSession = async () => {
+  //     const { data, error } = await supabase.auth.getSession();
+
+  //     if (error) throw error;
+
+  //     setSession(data);
+
+  //     supabase.auth.onAuthStateChange((_event, session) => {
+  //       setSession(session);
+  //     });
+  //   };
+  //   console.log("session", session);
+  //   fetchSession();
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <sessionContext.Provider value={session}>
+      <div className="App">{session ? <Notes /> : <Auth />}</div>
+    </sessionContext.Provider>
   );
-}
+};
 
 export default App;
