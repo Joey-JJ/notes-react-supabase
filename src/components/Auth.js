2357dd9ace3e399d7fec95bb393f6a0d841e3218
Joey van der Valk <joeyvandervalk@gmail.com>
2022-10-03 17:20:54 +0200

    Set up auth component, styling still todo

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signInWithOtp({ email });
+      if (error) throw error;
+      alert("Check your email for the login link!");
+    } catch (error) {
+      alert(error.error_description || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Please enter your e-mail address to sign in!</h1>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="email">E-mail address</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Enter here..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={loading}
+        />
+        <button type="submit" disabled={loading}>
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
